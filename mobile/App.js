c93badd8d072d3af44c6435be972a070a25ca1a6
Zhisong Ge <gzs253017369@gmail.com>
2019-09-09 00:27:42 -0400

    Bottom tab. I love bottom tab.

--- mobile/App.js
+++ mobile/App.js
@@ -5,35 +5,57 @@
  * @format
  * @flow
  */
-
 import React, { Component, useState } from 'react';
-import { Container, Header, Content, DatePicker, Text, View, Fab } from 'native-base';
+import { Container, Header, Content, DatePicker, Tab, Tabs, Text, View } from 'native-base';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 
-const DEFAULT_DATE = new Date(2017, 8, 28)
 const MIN_DATE = new Date(2015, 8, 17)
 const MAX_DATE = new Date(2019, 6, 8)
 const PLACE_HOLDER = "Select Date"
 
-const App = () => {
+const HomeScreen = () => {
   const [ chosenDate, setChosenDate ] = useState(Date.now())
 
   return (
-    <Container>
-      <Header />
-      <Content>
-        <DatePicker
-          defaultDate={new Date()}
-          minimumDate={MIN_DATE}
-          maximumDate={MAX_DATE}
-          locale={"en"}
-          placeHolderText={PLACE_HOLDER}
-          textStyle={{ color: "pink" }}
-          placeHolderTextStyle={{ color: "grey" }}
-          onDateChange={setChosenDate}
-        />
-      </Content>
-    </Container>
-  );
-};
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
 
-export default App;
+      <DatePicker
+        defaultDate={new Date()}
+        minimumDate={MIN_DATE}
+        maximumDate={MAX_DATE}
+        locale={"en"}
+        placeHolderText={PLACE_HOLDER}
+        textStyle={{ color: "pink" }}
+        placeHolderTextStyle={{ color: "grey" }}
+        onDateChange={setChosenDate}
+      />
+    </View>
+  );
+}
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+
+export default createAppContainer(
+  createBottomTabNavigator(
+    {
+      Home: { screen: HomeScreen },
+      Settings: { screen: SettingsScreen },
+    },
+    {
+      tabBarOptions: {
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      },
+    }
+  )
+);
