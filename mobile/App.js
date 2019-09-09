a0b0be1dbd834905de86ad75e0cdf05754862883
Zhisong Ge <gzs253017369@gmail.com>
2019-09-09 01:09:37 -0400

    small change.

--- mobile/App.js
+++ mobile/App.js
@@ -10,8 +10,6 @@ import { Container, Header, Content, DatePicker, Tab, Tabs, Text, View } from 'n
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs'
 
-const MIN_DATE = new Date(2015, 8, 17)
-const MAX_DATE = new Date(2019, 6, 8)
 const PLACE_HOLDER = "Select Date"
 
 const HomeScreen = () => {
@@ -23,8 +21,6 @@ const HomeScreen = () => {
 
       <DatePicker
         defaultDate={new Date()}
-        minimumDate={MIN_DATE}
-        maximumDate={MAX_DATE}
         locale={"en"}
         placeHolderText={PLACE_HOLDER}
         textStyle={{ color: "pink" }}
@@ -35,21 +31,19 @@ const HomeScreen = () => {
   );
 }
 
-class SettingsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
-    );
-  }
+const ReportsScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Reports!</Text>
+    </View>
+  );
 }
 
 export default createAppContainer(
   createBottomTabNavigator(
     {
       Home: { screen: HomeScreen },
-      Settings: { screen: SettingsScreen },
+      Reports: { screen: ReportsScreen },
     },
     {
       tabBarOptions: {
